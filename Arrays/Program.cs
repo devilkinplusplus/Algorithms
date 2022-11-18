68d0dfb0c5c0814d97fecdf1ee6883bc61216404
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2022-11-18 07:10:22 +0400

    18/11/2022

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,7 +14,7 @@ void InsertToArray<T>(ref T[] arr, T value, int index)
 
     for (int i = index; i < arr.Length; i++)
     {
-        newArray[i+1] = arr[i];
+        newArray[i + 1] = arr[i];
     }
     arr = newArray;
 }
@@ -28,7 +28,7 @@ foreach (var number in array)
 
 #region add value to last index of array
 Console.WriteLine();
-void AddLast<T>(ref T[] arr,T value)
+void AddLast<T>(ref T[] arr, T value)
 {
     T[] newArray = new T[array.Length + 1];
     newArray[arr.Length] = value;
@@ -36,7 +36,7 @@ void AddLast<T>(ref T[] arr,T value)
     {
         newArray[i] = arr[i];
     }
-  
+
     arr = newArray;
 }
 
@@ -49,12 +49,12 @@ foreach (var item in array)
 
 #region add value to first index of an array
 Console.WriteLine();
-void AddFirst<T>(ref T[] arr,T value)
+void AddFirst<T>(ref T[] arr, T value)
 {
     T[] newArray = new T[array.Length + 1];
     newArray[0] = value;
     int counter = 0;
-    for (int i = 1 ; i < arr.Length+1; i++)
+    for (int i = 1; i < arr.Length + 1; i++)
     {
         newArray[i] = arr[counter];
         counter++;
@@ -72,7 +72,7 @@ int[] array3 = { 213, 44, 512, 21, 56, 78 };
 
 #region Delete value from array
 //DeleteFromArray(array3,1);
-void DeleteFromArray<T>(ref T[] arr,int index)
+void DeleteFromArray<T>(ref T[] arr, int index)
 {
     T[] newArray = new T[arr.Length - 1];
 
@@ -80,9 +80,9 @@ void DeleteFromArray<T>(ref T[] arr,int index)
     {
         newArray[i] = arr[i];
     }
-    for (int i = index; i < arr.Length-1; i++)
+    for (int i = index; i < arr.Length - 1; i++)
     {
-        newArray[i] = arr[i+1];
+        newArray[i] = arr[i + 1];
 
     }
     arr = newArray;
@@ -101,11 +101,11 @@ int[] array2 = { 213, 44, 512, 21, 56, 78 };
 
 #region Nth smallest number
 
-int NthSmallest(int[] array,int n)
+int NthSmallest(int[] array, int n)
 {
-    for (int i = 0; i < array.Length-1; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        for (int j = 0; j < array.Length-1-i; j++)
+        for (int j = 0; j < array.Length - 1 - i; j++)
         {
             if (array[j + 1] < array[j])
             {
@@ -115,7 +115,19 @@ int NthSmallest(int[] array,int n)
             }
         }
     }
-    return array[n-1];
+    return array[n - 1];
 }
-Console.WriteLine("Nth smallest "+NthSmallest(array2,4));
+Console.WriteLine("Nth smallest " + NthSmallest(array2, 4));
+#endregion
+
+
+#region Ordered Count
+List<Tuple<char, int>> OrderedCount(string input)
+{
+    return input
+               .GroupBy(x => x)
+               .Select(x => Tuple.Create(x.Key, x.Count()))
+               .ToList();
+}
+Console.WriteLine(OrderedCount("abracadabra"));
 #endregion
